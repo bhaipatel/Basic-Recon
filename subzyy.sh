048894969aa008cc106c46a006e20f62c49aa372
bhaipatel <89388156+bhaipatel@users.noreply.github.com>
2022-03-01 16:33:30 +0530

    Add files via upload

---
+++ subzyy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+filename=$1
+while read line; do
+        echo "sufinder"
+         ~/go/bin/./subfinder -d $line -o $line.txt
+        echo "Assetfinder"
+        ~go/bin/./assetfinder --subs-only $line | tee -a $line.txt		
+        sort -u $line.txt -o $line.txt        
+        echo "Subjack"
+        subjack -w $line.txt -t 1000 -o $1takeover.txt
+done < $filename
+#./sudomainTakeover.sh subs.txt

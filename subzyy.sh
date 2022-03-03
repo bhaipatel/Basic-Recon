44bd0f283fd26e7e82837a425473c8cc0a635641
bhaipatel <89388156+bhaipatel@users.noreply.github.com>
2022-03-03 13:09:44 +0530

    Update subzyy.sh

--- subzyy.sh
+++ subzyy.sh
@@ -3,7 +3,12 @@ filename=$1
 while read line; do
         echo "sufinder"
          ~/go/bin/./subfinder -d $line -o $line.txt
-              
+         echo "Assetfinder"
+        ~/go/bin/./assetfinder --subs-only $line | tee -a $line.txt 
+        
+        sort -u $line.txt -o $line.txt
+        
+        
         echo "Subjack"
         ~/go/bin/./subjack -w $line.txt -t 1000 -o $1takeover.txt
 done < $filename

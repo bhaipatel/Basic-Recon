6f4420623ebdb06490b7fdfb87d7a948f277c209
bhaipatel <89388156+bhaipatel@users.noreply.github.com>
2022-03-05 10:12:34 +0530

    Update subzyy.sh

--- subzyy.sh
+++ subzyy.sh
@@ -1,15 +1,30 @@
 #!/bin/sh
 filename=$1
 while read line; do
-        echo "sufinder"
-         ~/go/bin/./subfinder -d $line -o $line.txt
-         echo "Assetfinder"
-        ~/go/bin/./assetfinder --subs-only $line | tee -a $line.txt 
+
+
+mkdir -p recon
+
+
         
-        sort -u $line.txt -o $line.txt
+~/go/bin/./subfinder -d $line -o $line.txt
+~/go/bin/./assetfinder --subs-only $line | tee -a $line.txt 
+curl -s "https://jldc.me/anubis/subdomains/"$line | grep -Po "((http|https):\/\/)?(([\w.-]*)\.([\w]*)\.([A-z]))\w+" >> $line
+curl -s "https://crt.sh/?q=%25."$line"&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' |tee $line.txt
+~/go/bin/./amass enum -d $line -passive|tee $line.txt 
+        
+
+
+
+
+sort -u $line.txt -o $line.txt
         
         
-        echo "Subjack"
-        ~/go/bin/./subjack -w $line.txt -t 1000 -o $1takeover.txt
+        
+       
+~/go/bin/./nuclei -list $line.txt -t ~/nuclei-templates/subdomain-takeover -o recon/$1takeover.txt
+
+
+
 done < $filename
 #./sudomainTakeover.sh subs.txt

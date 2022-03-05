6503bb86a8eedfc0a932d53c358b8d9a40e98c1d
bhaipatel <89388156+bhaipatel@users.noreply.github.com>
2022-03-05 10:23:06 +0530

    Update subzyy.sh

--- subzyy.sh
+++ subzyy.sh
@@ -8,14 +8,14 @@ mkdir -p recon
 echo -e "\n\e[36m[\e[32m+\e[36m]\e[92m Started Gathering Subdomain For $line \e[0m\n";
 
 
-        
+ {       
 ~/go/bin/./subfinder -d $line -o $line.txt
 ~/go/bin/./assetfinder --subs-only $line | tee -a $line.txt 
 curl -s "https://jldc.me/anubis/subdomains/"$line | grep -Po "((http|https):\/\/)?(([\w.-]*)\.([\w]*)\.([A-z]))\w+" >> $line
 curl -s "https://crt.sh/?q=%25."$line"&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' |tee $line.txt
 ~/go/bin/./amass enum -d $line -passive|tee $line.txt 
         
-
+} &> /dev/nul
 
 
 
